77126a2222d4d60fba494534df4c1517a93728b4
Daniel <rlondono@bancolombia.com.co>
2019-05-26 00:26:11 -0500

    Se agrega footer y header

--- AppEPortes/src/app/app.module.ts
+++ AppEPortes/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './shared/home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
