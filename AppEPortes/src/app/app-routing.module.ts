dd4d14c45dd0d5c67ee7d3288c6230bd69b1b6ce
Daniel <rlondono@bancolombia.com.co>
2019-05-26 23:42:52 -0500

    Se agregan login y rutas

--- AppEPortes/src/app/app-routing.module.ts
+++ AppEPortes/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './shared/login/login.component';
+import { HomeComponent } from './shared/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'auth',component:LoginComponent},
+  {path:'home', component:HomeComponent},
+  {path:'**',pathMatch:'full', redirectTo :'auth'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AppEPortes/src/app/app.module.ts
+++ AppEPortes/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './shared/home/home.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { LoginComponent } from './shared/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
